27e01e4ad03d06c9dfc283b908f492cf6c67f9fd
eben-k <ennim.consults@gmail.com>
2020-10-19 01:25:48 +0100

    feat(dashboad): -creates profile dashboard view

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,25 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
-    </v-main>
+    <div id="app">
+      <router-view />
+    </div>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld";
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
 
-export default {
-  name: "App",
+body {
+  font-family: "Lato";
+  background: #e5e5e5;
+  width: 120rem;
+  box-sizing: border-box;
+}
 
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
-};
-</script>
+html {
+  font-size: 62.5%;
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="sidebar-container">
+    <div class="bottom-logo-box">
+      <v-img src="../assets/mzuri.svg"></v-img>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar"
+};
+</script>
+<style lang="scss" scoped>
+.sidebar-container {
+  .bottom-logo-box {
+    background: #ffffff;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 2px 3.4rem;
+    margin-bottom: 0;
+    align-self: end;
+  }
+}
+</style>
